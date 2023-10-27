d1775c1946a36830996a59d34abdf57c687ac27a
Beatriz Gonçalves de Souza <102838758+Beatrizgs1@users.noreply.github.com>
2023-10-26 22:01:09 -0300

    exercicio 4

--- exerciciosDeFixacao20231026.sql
+++ exerciciosDeFixacao20231026.sql
@@ -1,5 +1,5 @@
 --EXERCÍCIOS DE FIXAÇÃO:
---Crie um trigger que, após inserir um novo cliente na tabela Clientes, insira uma mensagem na tabela Auditoria informando a data e hora da inserção.
+--1.Crie um trigger que, após inserir um novo cliente na tabela Clientes, insira uma mensagem na tabela Auditoria informando a data e hora da inserção.
 
 DELIMITER //
 CREATE TRIGGER insere_cliente
@@ -13,7 +13,7 @@ END;
 DELIMITER ;
 
 
---Antes de excluir um cliente da tabela Clientes, crie um trigger que insira uma mensagem na tabela Auditoria informando sobre a tentativa de exclusão.
+--2.Antes de excluir um cliente da tabela Clientes, crie um trigger que insira uma mensagem na tabela Auditoria informando sobre a tentativa de exclusão.
 
 DELIMITER //
 CREATE TRIGGER tentativa_exclusao_cliente
@@ -27,7 +27,7 @@ END;
 DELIMITER ;
 
 
---Após atualizar o nome de um cliente na tabela Clientes, insira uma mensagem na tabela Auditoria mostrando o nome antigo e o novo nome.
+--3.Após atualizar o nome de um cliente na tabela Clientes, insira uma mensagem na tabela Auditoria mostrando o nome antigo e o novo nome.
 
 DELIMITER //
 CREATE TRIGGER atualiza_nome_cliente
@@ -36,6 +36,23 @@ FOR EACH ROW
 BEGIN
     INSERT INTO Auditoria (mensagem)
     VALUES (CONCAT('Nome do cliente ID ', OLD.id, ' atualizado de "', OLD.nome, '" para "', NEW.nome, '" em ', NOW()));
+END;
+//
+DELIMITER ;
+
+
+--4.Não permita que o nome do cliente seja atualizado para uma string vazia ou NULL. Se alguém tentar fazer isso, o trigger deve impedir a atualização e inserir uma mensagem na tabela Auditoria.
+
+DELIMITER //
+CREATE TRIGGER impede_nome_vazio_ou_null
+BEFORE UPDATE ON Clientes
+FOR EACH ROW
+BEGIN
+    IF NEW.nome IS NULL OR NEW.nome = '' THEN
+        INSERT INTO Auditoria (mensagem)
+        VALUES ('Tentativa de atualizar nome para vazio ou NULL impedida em ' NOW());
+        SET NEW.nome = OLD.nome;
+    END IF;
 END;
 //
 DELIMITER ;
